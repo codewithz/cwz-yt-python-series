50dfcca6d04864ca6e81f77f8544a53a9c7a2fb9
Zartab Nakhwa <zartab@codewithz.com>
2024-07-14 13:30:56 +0530

    cwz-yt-python-series

---
+++ data-types.py
@@ -0,0 +1,70 @@
+x='Code With Z'
+print(x)
+print(type(x))
+
+print("-----------------------------")
+
+x=10
+print(x)
+print(type(x))
+# Select the lines you want to repeat
+# Shift + Alt + Down Arrow 
+print("-----------------------------")
+
+x=10.45
+print(x)
+print(type(x))
+# complex Number --> real number + imaginary numbers 'j'
+print("-----------------------------")
+
+x=10+5j
+print(x)
+print(type(x))
+print("-----------------------------")
+
+x=False
+print(x)
+print(type(x))
+
+# -----------------------
+# Sequqnce Type 
+# List -- [] -- mutable
+# Tuple -- () -- immutable
+# Range -- range(from,to) -- iterable 
+
+print("-----------------------------")
+
+x=['Honda','Toyota','Nissan','Skoda','Chevrolet']
+print(x)
+print(type(x))
+
+print("-----------------------------")
+
+x=('Honda','Toyota','Nissan','Skoda','Chevrolet')
+print(x)
+print(type(x))
+
+print("-----------------------------")
+
+x=range(1,10)
+print(x)
+print(type(x))
+
+# Set and Dictionary 
+# Set -- {} -- no duplicates | Unordered
+
+print("-----------------------------")
+
+x={'Honda','Toyota','Nissan','Skoda','Chevrolet','Nissan'}
+print(x)
+print(type(x))
+
+# Dictionary 
+# dict --- {key:value} -- key -- set | value --list
+
+print("-----------------------------")
+
+x={'name':'Zartab Nakhwa','blog':'www.codewithz.com','youtube':'Code With Z','city':'Mumbai'}
+print(x)
+print(type(x))
+
